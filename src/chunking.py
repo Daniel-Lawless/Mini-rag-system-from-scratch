1d7a289a610ff48a9f0ff40c028a6b3c02b8600d
dxl397 <dxl397@student.bham.ac.uk>
2026-05-19 23:07:13 +0100

    Added the chunker and the embedder

---
+++ src/chunking.py
@@ -0,0 +1,23 @@
+class Chunking:
+
+    def chunk_text(self, text: str, chunk_size: int = 20, overlap:int = 5) -> list[str]:
+
+        # Overlap cannot be greater than or equal than chunk_size
+        if overlap >= chunk_size:
+            raise ValueError("Overlap cannot be larger than chunk size")
+
+        words = text.split() # Splits our text into a list of words
+        start = 0            # Starts at the first word
+        chunks = []          # Stores each chunk
+
+        while start < len(words):
+            end = start + chunk_size
+            chunk = " ".join(words[start:end]) # ["Daniel", "is", "happy"] -> "Daniel is happy"
+            chunks.append(chunk)
+
+            # Overlap helps avoid missing context, especially when an important sentence,
+            # definition, equation, or explanation is split between chunks.
+            start += chunk_size - overlap 
+        
+        # Return the text split into chunks
+        return chunks
---
+++ src/embeddings.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+class Embeddings:
+
+    def __init__(self):
+
+        # Model to embed strings.
+        self.model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+    def embed(self, text: str) -> np.ndarray:
+
+        # Embed the vector
+        vector_embedding = self.model.encode(text, convert_to_numpy=True)
+
+        # Normalise the vector embedding so is it's just the dot product for cosine similarity search.
+        vector_embed_normalized = vector_embedding / np.linalg.norm(vector_embedding)
+
+        # Return the normalized embeded vector.
+        return vector_embed_normalized
